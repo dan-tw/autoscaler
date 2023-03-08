d9e1de77adffc9c4b1aae0226d8255f3a8ea4e72
Daniel Holden <daniel@tensorworks.com.au>
2023-03-08 16:42:48 +1000

    update container to use latest tag of the forked cluster autoscaler

--- cluster-autoscaler/docker_build.sh
+++ cluster-autoscaler/docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t dpholden/cluster-autoscaler:1.25.0-alpha.2 -f Dockerfile.amd64 .
-docker push dpholden/cluster-autoscaler:1.25.0-alpha.2
+docker build -t dpholden/cluster-autoscaler:1.25.0-alpha.2-v0.0.1 -f Dockerfile.amd64 .
+docker push dpholden/cluster-autoscaler:1.25.0-alpha.2-v0.0.1
